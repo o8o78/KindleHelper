85d571af3a833a29dd729f45b199e539de32dc4d
zhichao yang <zhichao.yang@backgardon.com>
2022-07-11 11:20:01 +0800

    --initialization

--- app/src/main/java/com/byteroll/kindlehelper/MainActivity.kt
+++ app/src/main/java/com/byteroll/kindlehelper/MainActivity.kt
@@ -2,18 +2,29 @@ package com.byteroll.kindlehelper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.byteroll.kindlehelper.databinding.ActivityMainBinding
 import com.byteroll.kindlehelper.utils.Utils
+import com.byteroll.kindlehelper.viewmodels.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var viewModel: MainViewModel
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         init()
     }
 
     private fun init(){
         Utils.setUiFlags(this)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
     }
+
+
 
 }
---
+++ app/src/main/java/com/byteroll/kindlehelper/viewmodels/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.byteroll.kindlehelper.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+
+
+}
