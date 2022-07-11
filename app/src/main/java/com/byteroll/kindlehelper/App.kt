35c1a58b5fe46337f07aa2766af8f1d8f422dfa5
zhichao yang <zhichao.yang@backgardon.com>
2022-07-11 11:05:30 +0800

    --global context access enabled

---
+++ app/src/main/java/com/byteroll/kindlehelper/App.kt
@@ -0,0 +1,19 @@
+package com.byteroll.kindlehelper
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+class App : Application() {
+
+    companion object{
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+
+}
