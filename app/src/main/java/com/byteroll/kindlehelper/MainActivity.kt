5e1ef28817d195bc6271bf83c9819fb48da7c688
zhichao yang <zhichao.yang@backgardon.com>
2022-07-11 17:34:36 +0800

    --MainActivity应用缓存

--- app/src/main/java/com/byteroll/kindlehelper/MainActivity.kt
+++ app/src/main/java/com/byteroll/kindlehelper/MainActivity.kt
@@ -20,19 +20,6 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private val articles = mutableListOf(
-        Article(
-            "Game of throne",
-            "There were fierce battles in gaza on Friday as Hamas and Israel both rejected the United Nations' call for cease-fire."
-        ),
-        Article(
-            "Play of the UnNamed",
-            "How does a bastard, orphan, son of a whole and Scottman, dropped in the middle of Caribean by province in squalr, grow up to be a hero and scolar?"
-        )
-    )
-
-    private val articleList = ArrayList<Article>()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -58,19 +45,11 @@ class MainActivity : AppCompatActivity() {
         Utils.setUiFlags(this)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         setSupportActionBar(binding.toolbar)
-        initArticles()
+        viewModel.initArticles()
         val layoutManager = GridLayoutManager(this, 1)
         binding.recyclerView.layoutManager = layoutManager
-        val adapter = HomeListAdapter(this, articleList)
+        val adapter = HomeListAdapter(this, viewModel.articleList)
         binding.recyclerView.adapter = adapter
-    }
-
-    private fun initArticles() {
-        articleList.clear()
-        repeat(50) {
-            val index = (0 until 1).random()
-            articleList.add(articles[index])
-        }
     }
 
 }
