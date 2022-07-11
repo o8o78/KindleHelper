60daeb7759810218beb6d4297d719b45e57f8128
zhichao yang <zhichao.yang@backgardon.com>
2022-07-11 11:19:20 +0800

    --日志工具

--- app/src/main/java/com/byteroll/kindlehelper/utils/Utils.kt
+++ app/src/main/java/com/byteroll/kindlehelper/utils/Utils.kt
@@ -2,7 +2,7 @@ package com.byteroll.kindlehelper.utils
 
 import android.app.Activity
 import android.graphics.Color
-import android.view.VerifiedInputEvent
+import android.util.Log
 import android.view.View
 
 class Utils {
@@ -15,6 +15,25 @@ class Utils {
                     View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE
             }
+        }
+    }
+}
+
+private val LOG_MAX_LENGTH = 3 * 1024
+
+fun String.log(){
+    val tag = "AppLog"
+    val strLength = this.length
+    var start = 0
+    var end = LOG_MAX_LENGTH
+    for (i in 0..100){
+        if (strLength > end){
+            Log.d(tag + i, this.substring(start, end))
+            start = end
+            end += LOG_MAX_LENGTH
+        } else{
+            Log.d(tag+i, this.substring(start, strLength))
+            break
         }
     }
 }
