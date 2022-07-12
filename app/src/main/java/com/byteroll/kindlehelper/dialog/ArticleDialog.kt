4cbe76346fa931870446e2cc42f8d87e6b1e2699
zhichao yang <zhichao.yang@backgardon.com>
2022-07-12 16:34:25 +0800

    --ArticleDialog全屏显示

--- app/src/main/java/com/byteroll/kindlehelper/dialog/ArticleDialog.kt
+++ app/src/main/java/com/byteroll/kindlehelper/dialog/ArticleDialog.kt
@@ -1,13 +1,18 @@
 package com.byteroll.kindlehelper.dialog
 
+import android.app.Activity
 import android.app.Dialog
 import android.content.Context
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import com.byteroll.kindlehelper.R
 import com.byteroll.kindlehelper.databinding.DlgArticleBinding
+import com.byteroll.kindlehelper.utils.Utils
 
 class ArticleDialog(context: Context) : Dialog(context, R.style.TransDialog), View.OnClickListener {
 
@@ -15,23 +20,45 @@ class ArticleDialog(context: Context) : Dialog(context, R.style.TransDialog), Vi
 
     private var article: String? = null
 
-    constructor(context: Context, article: String): this(context){
+    private var activity: Activity? = null
+
+    constructor(context: Context, article: String) : this(context) {
         this.article = article
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         init()
+        setUIFlags()
         initWidget()
         initComponents()
     }
 
-    private fun init(){
+    private fun init() {
         binding = DlgArticleBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        activity = Utils.getActivityOfContext(context)
     }
 
-    private fun initWidget(){
+    private fun setUIFlags() {
+        activity?.let {
+            window?.let {
+                it.decorView.systemUiVisibility =
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                            View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                it.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+                it.statusBarColor = Color.TRANSPARENT
+                val lp = it.attributes
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P){
+                    lp.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+                }
+                it.attributes = lp
+            }
+        }
+    }
+
+    private fun initWidget() {
         window?.let {
             val params = it.attributes
             params.width = ViewGroup.LayoutParams.MATCH_PARENT
@@ -41,17 +68,17 @@ class ArticleDialog(context: Context) : Dialog(context, R.style.TransDialog), Vi
         }
     }
 
-    private fun initComponents(){
-        binding.apply{
+    private fun initComponents() {
+        binding.apply {
             containerShow.text = article
             back.setOnClickListener(this@ArticleDialog)
         }
     }
 
     override fun onClick(v: View?) {
-        with(binding){
-            when(v){
-                back ->{
+        with(binding) {
+            when (v) {
+                back -> {
                     dismiss()
                 }
             }
